06ae923660ae6ba44a306f0a900cc3ebbc4dcf3b
KIOSHIROI <127948158+KIOSHIROI@users.noreply.github.com>
2024-10-26 21:45:09 +0800

    Delete old_files directory

--- old_files/old_test/test7.py
+++
@@ -1,4 +0,0 @@
-from pyEnergy.fool import initialize_with_feature_selector
-
-fool = initialize_with_feature_selector("data/ChangErZhai-40-139079-values 20180101-20181031.csv", method="pca")
-print(*fool.features())
